d2137b4fdc16eb05454da1f319c9ef7c79563ab1
Aman Gupta <amangupta99344@gmail.com>
2022-06-18 20:17:35 +0530

    java_practice

---
+++ June_17_2022/LoopPractice.java
@@ -0,0 +1,21 @@
+package June_17_2022;
+
+public class LoopPractice {
+    public static void main(String[] args) {
+
+        
+        // for(int i = 0; i<3; i++){
+        //     System.out.println("*");
+        // }
+
+        for(int i = 0; i <2; i++){
+            for(int j = 10; j<=12; j++){
+                System.out.print( "Hello world");
+
+            }
+           
+        }
+
+    }
+}
+
---
+++ June_17_2022/Pattern2.java
@@ -0,0 +1,12 @@
+package June_17_2022;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            for(int j = 2; j<=i; j++){
+                System.out.print(i + " " );
+            }
+            System.out.println(i);
+        }
+    }
+}
